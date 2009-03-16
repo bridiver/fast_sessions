a936b3957b6f541a078923a97f771242fe93210a
Brian Johnson <github@brianjohnson.cc>
2009-03-16 13:26:59 -0700

    updates for rails 2.3

--- spec/fast_sessions_spec.rb
+++ spec/fast_sessions_spec.rb
@@ -1,23 +1,23 @@
 require File.dirname(__FILE__) + '/spec_helper'
 require 'fast_sessions'
 
-describe "CGI::Session::ActiveRecordStore::FastSessions Class" do
+describe "ActiveRecord::SessionStore::FastSessions Class" do
   it "should have connection attribute" do
     connection = mock("connection")
-    CGI::Session::ActiveRecordStore::FastSessions.connection = connection
-    CGI::Session::ActiveRecordStore::FastSessions.connection.should be(connection)
+    ActiveRecord::SessionStore::FastSessions.connection = connection
+    ActiveRecord::SessionStore::FastSessions.connection.should be(connection)
   end
 
   it "should have table_name attribute" do
-    CGI::Session::ActiveRecordStore::FastSessions.table_name = "table_name"
-    CGI::Session::ActiveRecordStore::FastSessions.table_name.should == "table_name"
+    ActiveRecord::SessionStore::FastSessions.table_name = "table_name"
+    ActiveRecord::SessionStore::FastSessions.table_name.should == "table_name"
   end
 
   it "should correctly marshal/unmarshal data" do
     data = [ "some", { :test => 'data', :structure => 1}, "to", :check, 'serializat', 10, "n" ]
-    marshaled_data = CGI::Session::ActiveRecordStore::FastSessions.marshal(data)
+    marshaled_data = ActiveRecord::SessionStore::FastSessions.marshal(data)
     marshaled_data.should_not be(nil)
-    CGI::Session::ActiveRecordStore::FastSessions.unmarshal(marshaled_data).should == data
+    ActiveRecord::SessionStore::FastSessions.unmarshal(marshaled_data).should == data
   end
 end
 
@@ -25,33 +25,33 @@ describe "FashSessions Class find_by_session_id() method" do
   before(:each) do
     @connection = mock("connection")
     @connection.stub!(:quote).and_return("something")
-    CGI::Session::ActiveRecordStore::FastSessions.connection = @connection
-    CGI::Session::ActiveRecordStore::FastSessions.fallback_to_old_table = false
+    ActiveRecord::SessionStore::FastSessions.connection = @connection
+    ActiveRecord::SessionStore::FastSessions.fallback_to_old_table = false
 
     @data = { :test => "value" }
-    @marshaled_data = CGI::Session::ActiveRecordStore::FastSessions.marshal(@data)
+    @marshaled_data = ActiveRecord::SessionStore::FastSessions.marshal(@data)
   end
   
-  it "should return a CGI::Session::ActiveRecordStore::FastSessions object with saved data when called for existing session" do
+  it "should return a ActiveRecord::SessionStore::FastSessions object with saved data when called for existing session" do
     @connection.should_receive(:select_one).and_return({'data' => @marshaled_data})
-    session = CGI::Session::ActiveRecordStore::FastSessions.find_by_session_id("test_id")
-    session.class.should be(CGI::Session::ActiveRecordStore::FastSessions)
+    session = ActiveRecord::SessionStore::FastSessions.find_by_session_id("test_id")
+    session.class.should be(ActiveRecord::SessionStore::FastSessions)
     session.data.should == @data
   end
 
-  it "should return a CGI::Session::ActiveRecordStore::FastSessions object with empty hash when called for non-existing session" do
+  it "should return a ActiveRecord::SessionStore::FastSessions object with empty hash when called for non-existing session" do
     @connection.should_receive(:select_one).and_return(nil)
-    session = CGI::Session::ActiveRecordStore::FastSessions.find_by_session_id("test_id")
-    session.class.should be(CGI::Session::ActiveRecordStore::FastSessions)
+    session = ActiveRecord::SessionStore::FastSessions.find_by_session_id("test_id")
+    session.class.should be(ActiveRecord::SessionStore::FastSessions)
     session.data.should be_empty
   end
 
   it "should fallback to the old sessions table if session was not found in new one" do
     @connection.should_receive(:select_one).twice.and_return(nil, {'data' => @marshaled_data})
-    CGI::Session::ActiveRecordStore::FastSessions.fallback_to_old_table = true
+    ActiveRecord::SessionStore::FastSessions.fallback_to_old_table = true
 
-    session = CGI::Session::ActiveRecordStore::FastSessions.find_by_session_id("test_id")
-    session.class.should be(CGI::Session::ActiveRecordStore::FastSessions)
+    session = ActiveRecord::SessionStore::FastSessions.find_by_session_id("test_id")
+    session.class.should be(ActiveRecord::SessionStore::FastSessions)
     session.data.should == @data
   end
 end
@@ -59,15 +59,15 @@ end
 describe "FashSessions object save() method" do
   before(:each) do
     @data = { :test => "value" }
-    @marshaled_data = CGI::Session::ActiveRecordStore::FastSessions.marshal(@data)
+    @marshaled_data = ActiveRecord::SessionStore::FastSessions.marshal(@data)
 
     @connection = mock("connection")
     @connection.stub!(:quote).and_return("something")
     @connection.should_receive(:select_one).and_return({'data' => @marshaled_data})
-    CGI::Session::ActiveRecordStore::FastSessions.connection = @connection
-    CGI::Session::ActiveRecordStore::FastSessions.fallback_to_old_table = false
+    ActiveRecord::SessionStore::FastSessions.connection = @connection
+    ActiveRecord::SessionStore::FastSessions.fallback_to_old_table = false
     
-    @session = CGI::Session::ActiveRecordStore::FastSessions.find_by_session_id("test_id")
+    @session = ActiveRecord::SessionStore::FastSessions.find_by_session_id("test_id")
   end
   
   it "should not save data if should_save_session? returns false" do
@@ -137,17 +137,17 @@ end
 describe "FashSessions object save() method in special cases" do
   before(:each) do
     @data = { :test => "value" }
-    @marshaled_data = CGI::Session::ActiveRecordStore::FastSessions.marshal(@data)
+    @marshaled_data = ActiveRecord::SessionStore::FastSessions.marshal(@data)
 
     @connection = mock("connection")
     @connection.stub!(:quote).and_return("something")
-    CGI::Session::ActiveRecordStore::FastSessions.connection = @connection
-    CGI::Session::ActiveRecordStore::FastSessions.fallback_to_old_table = false
+    ActiveRecord::SessionStore::FastSessions.connection = @connection
+    ActiveRecord::SessionStore::FastSessions.fallback_to_old_table = false
   end
 
   it "should not save data if the only thing added to an empty session was a blank flash message" do
     @connection.should_receive(:select_one).and_return(nil)
-    @session = CGI::Session::ActiveRecordStore::FastSessions.find_by_session_id("another_id")
+    @session = ActiveRecord::SessionStore::FastSessions.find_by_session_id("another_id")
 
     @connection.should_not_receive(:update)
     @session.data["flash"] = {}
